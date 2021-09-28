0b4ef8d4de6a53f95058eaec68a22cbaf173a2ab
j6512 <justins8287@gmail.com>
2021-09-28 01:09:05 -0500

    beginning

---
+++ employee-manager-springboot-angular/src/main/java/com/example/employeemanagerspringbootangular/EmployeeManagerSpringbootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagerspringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerSpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerSpringbootAngularApplication.class, args);
+	}
+
+}
